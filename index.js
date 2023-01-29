a2d0d70bb3af7b70d943b4f1f8d612c1fb0aa551
fukudamiyasato <fukuda.miyasato@gmail.com>
2023-01-28 23:35:45 -0500

    second commit

--- index.js
+++ index.js
@@ -2,15 +2,36 @@ var express = require("express");
 var app = express();
 
 //TODO: create a redis client
+var redis = require('redis');
+var client = redis.createClient();
 
 // serve static files from public directory
 app.use(express.static("public"));
 
 // TODO: initialize values for: header, left, right, article and footer using the redis client
-
+client.mset('header',0,'left',0,'article',0,'right',0,'footer',0);
+client.mget(['header','left','article','right','footer'],
+    function(err,value){
+        console.log(value);
+    });
 // Get values for holy grail layout
 function data() {
   // TODO: uses Promise to get the values for header, left, right, article and footer from Redis
+  return new Promise((resolve, reject) =>{
+    client.mget(['header','left','article','right','footer'],
+      function(err, value){
+        const data = {
+          'header': Number(value[0]),
+          'left': Number(value[0]),
+          'article': Number(value[0]),
+          'right': Number(value[0]),
+          'footer': Number(value[0])
+
+        };
+        err? reject(null) : resolve( data);
+      }
+    );
+  });
 }
 
 // plus
@@ -19,6 +40,14 @@ app.get("/update/:key/:value", function (req, res) {
   let value = Number(req.params.value);
 
   //TODO: use the redis client to update the value associated with the given key
+  client.get(key, function(err, reply){
+    value = Number(reply) + value;
+    client.set(key,value);
+    data().then(data =>{
+      console.log(data);
+      res.send(data);
+    });
+  });
 });
 
 // get key data
